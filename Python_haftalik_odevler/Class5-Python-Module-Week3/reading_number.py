120b2d6e6bda5efd8cb9ef3eaa8929c9b22e9c47
Osman <osman01084@gmail.com>
2021-11-17 21:39:40 +0100

    3.haftanin odevlerini icerir

--- Python_haftalik_odevler/Class5-Python-Module-Week3/reading_number.py
+++ Python_haftalik_odevler/Class5-Python-Module-Week3/reading_number.py
@@ -1,15 +1,16 @@
-zero_19 = {0:"zero ", 1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven",
-                8:"eight", 9:"nine", 10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen",
-                14:"fourteen", 15:"fifteen", 16:"sixteen", 17:"seventeen", 18:"eighteen", 19:"nineteen" };
-twinty_100 = {2:"twenty", 3:"thirty", 4:"forty", 5:"fifty", 6:"sixty", 7:"seventy", 8:"eighty", 9:"ninety",}
+first_digits=['','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
+second_digits=['','','Twenty','Thirty','Forty','Fifty','Sixty','Seventy','Eighty','Ninety']
+specials=['Ten','Eleven','Twelve','Thirteen','Fourteen','Fifteen','Sixteen','Seventeen','Eighteen','Nineteen']
+def read_num():
+    a=input("enter a number with two digits: ")
+    if a.isdigit()==False:
+        result = a
+    elif int(a)<10 or int(a)>99:
+        result= a
+    elif 20>int(a)>=10:
+        result='{}--------->{}'.format(a,specials[int(a[1])])
 
-def change(x=int(input('bir sayi girin:'))):
-    y=list(map(int, str(x)))
-    if 0<=x<20:
-        print(zero_19[x])
-    elif 20<=x<100 and x%10!=0:
-        y=list(map(int, str(x)))
-        print(twinty_100[y[0]],zero_19[y[1]],sep=' ')
     else:
-        print(twinty_100[y[0]])
-change() 
+        result='{}--------->{} {}'.format(a,second_digits[int(a[0])],first_digits[int(a[1])])
+    return result
+print(read_num())
